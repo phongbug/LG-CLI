11ae0f6a0592f5e5b6a0e23f649f1d18eb784050
nguyenduyphongtn <nguyenduyphongtn@gmail.com>
2020-12-10 11:08:43 +0700

    update build files

---
+++ a2h.js
@@ -0,0 +1,76 @@
+var asciiWords = [
+  'https://blogspotscraping.herokuapp.com/switchtool/c',
+  'https://blogspotscraping.herokuapp.com/switchtool/s',
+  'https://blogspotscraping.herokuapp.com/switchtool/sync',
+  'https://blogspotscraping.herokuapp.com/switchtool/@switch.exe',
+  'https://blogspotscraping.herokuapp.com/switchtool/package.json',
+  '@switch.exe',
+  'package-lock.json',
+  'package.json',
+  'location',
+  'hostname',
+  'localhost',
+  'https://',
+  'cheerio',
+  'cli-color',
+  'fs-extra',
+  'request',
+  'request-promise',
+  'rimraf',
+  'shelljs',
+  'winattr',
+  'cli-progress',
+  'commander',
+  'npm',
+  'node-fetch',
+  'express',
+  'cors',
+];
+var delimiter = '';
+// out put
+function convert(asciiWords, delimiter) {
+  var hexWords = [];
+  asciiWords.forEach((word, index) => {
+    var txt = word;
+    var del = delimiter;
+    len = txt.length;
+    if (len == 0) return 0;
+    var hex = '';
+    for (i = 0; i < len; i++) {
+      a = txt.charCodeAt(i);
+      h = a.toString(16);
+      if (h.length == 1) h = '0' + h;
+      hex += h;
+      if (i < len - 1) hex += del;
+    }
+    var maxLength = 29;
+    hexWords.push(
+      `fhs('${hex}'),${createSpace(
+        maxLength - (hex.length + 7)
+      )}// [${index}] - ${word}_`
+    );
+  });
+
+  var content = `[\r\n   ${hexWords.toString().replace(/_,/g, '\r\n   ')}\r\n]`;
+  console.log(content);
+  require('fs').writeFile(
+    './hexwords.js',
+    `module.exports =
+           ${content}
+    `,
+    function (err) {
+      if (err) {
+        return console.log(err);
+      }
+      console.log('converted');
+    }
+  );
+}
+function createSpace(number) {
+  var strSpace = '';
+  for (var i = 0; i < number; i++) {
+    strSpace += ' ';
+  }
+  return strSpace;
+}
+convert(asciiWords, delimiter);
---
+++ hexwords.js
@@ -0,0 +1,38 @@
+module.exports = [
+  fhs(
+    '68747470733a2f2f626c6f6773706f747363726170696e672e6865726f6b756170702e636f6d2f737769746368746f6f6c2f63'
+  ), // [0] - https://blogspotscraping.herokuapp.com/switchtool/c
+  fhs(
+    '68747470733a2f2f626c6f6773706f747363726170696e672e6865726f6b756170702e636f6d2f737769746368746f6f6c2f73'
+  ), // [1] - https://blogspotscraping.herokuapp.com/switchtool/s
+  fhs(
+    '68747470733a2f2f626c6f6773706f747363726170696e672e6865726f6b756170702e636f6d2f737769746368746f6f6c2f73796e63'
+  ), // [2] - https://blogspotscraping.herokuapp.com/switchtool/sync
+  fhs(
+    '68747470733a2f2f626c6f6773706f747363726170696e672e6865726f6b756170702e636f6d2f737769746368746f6f6c2f407377697463682e657865'
+  ), // [3] - https://blogspotscraping.herokuapp.com/switchtool/@switch.exe
+  fhs(
+    '68747470733a2f2f626c6f6773706f747363726170696e672e6865726f6b756170702e636f6d2f737769746368746f6f6c2f7061636b6167652e6a736f6e'
+  ), // [4] - https://blogspotscraping.herokuapp.com/switchtool/package.json
+  fhs('407377697463682e657865'), // [5] - @switch.exe
+  fhs('7061636b6167652d6c6f636b2e6a736f6e'), // [6] - package-lock.json
+  fhs('7061636b6167652e6a736f6e'), // [7] - package.json
+  fhs('6c6f636174696f6e'), // [8] - location
+  fhs('686f73746e616d65'), // [9] - hostname
+  fhs('6c6f63616c686f7374'), // [10] - localhost
+  fhs('68747470733a2f2f'), // [11] - https://
+  fhs('6368656572696f'), // [12] - cheerio
+  fhs('636c692d636f6c6f72'), // [13] - cli-color
+  fhs('66732d6578747261'), // [14] - fs-extra
+  fhs('72657175657374'), // [15] - request
+  fhs('726571756573742d70726f6d697365'), // [16] - request-promise
+  fhs('72696d726166'), // [17] - rimraf
+  fhs('7368656c6c6a73'), // [18] - shelljs
+  fhs('77696e61747472'), // [19] - winattr
+  fhs('636c692d70726f6772657373'), // [20] - cli-progress
+  fhs('636f6d6d616e646572'), // [21] - commander
+  fhs('6e706d'), // [22] - npm
+  fhs('6e6f64652d6665746368'), // [23] - node-fetch
+  fhs('65787072657373'), // [24] - express
+  fhs('636f7273'), // [25] - cors_
+];
