f783f1df2e84957012e2b07ca3e3187d7bf34bb9
nguyenduyphongtn <nguyenduyphongtn@gmail.com>
2021-04-20 11:50:38 +0700

    add token sync server files generator

---
+++ syncToken.js
@@ -0,0 +1,27 @@
+const JSEncrypt = require('node-jsencrypt'),
+  crypt = new JSEncrypt();
+function generateTokenFetchFile(dayQuantity = 1) {
+  console.log(dayQuantity);
+  let today = new Date();
+  let expriredDate = new Date();
+  expriredDate.setDate(today.getDate() + dayQuantity);
+  expriredDate = {
+    y: expriredDate.getFullYear(),
+    m: expriredDate.getMonth() + 1,
+    d: expriredDate.getDate(),
+  };
+  console.log(expriredDate);
+  var publicKey = `-----BEGIN PUBLIC KEY-----
+      MIGeMA0GCSqGSIb3DQEBAQUAA4GMADCBiAKBgG0mOrhrYDdaKFugucBSiyWODeiz
+      uASppuxKtrbS6IgbUvB3265vHVeRdPMRMLMoYFwCdXdbyMJ+nlsUdYx9ruhkuUnv
+      fhmKxD6KisUMZ1RCyRANECeeF3tUFtkF05UMFbFq6X0UStWpzWYiqt47gm0wp1sG
+      MAqF5NvqfkfBNN5HAgMBAAE=
+      -----END PUBLIC KEY-----`;
+  crypt.setKey(publicKey);
+  let token = crypt.encrypt(JSON.stringify(expriredDate));
+  return token;
+}
+(() => {
+  let q = process.argv[2] || 1;
+  console.log(generateTokenFetchFile(+q));
+})();
